183c4a428c2b11138a54a1d40dafe0b495659f7b
Tony Macas <iamtonymacas@gmail.comi>
2018-04-06 15:37:51 -0400

    added materilaize

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+// global variable for pokemon
+let pokeAll = [];
+
+
+let displayedPokemon = document.querySelector(".poke-image");
+let pokeName = document.querySelector(".name1");
+let pokeHp1 = document.querySelector(".hp1");
+let pokeAttack1 = document.querySelector(".attack1")
+let pokeDefense1 = document.querySelector(".defense1");
+let pokeAbi1 = document.querySelector(".abi1-1");
+let pokeAbi2 = document.querySelector(".abi1-2");
+
+let crobatTarget = document.querySelector("#firstball");
+let croconawTarget = document.querySelector("#secondball");
+let crustleTarget = document.querySelector('#thirdball');
+
+console.log(displayedPokemon);
+
 // container class
-class People {
+class Tony {
   constructor() {
     this.all = []
   }
@@ -7,114 +25,231 @@ class People {
   // defines method add to People class
   // it accepts a Person object and adds it to the end
   // of the "all" array
-  add(person) {
-    this.all.push(person)
+  add(pokeData) {
+    this.all.push(pokeData)
   }
 }
 
 // individual class
 class Pokemon {
-  constructor(name, hp, attack, defense, abilities) {
+  constructor(name, hp, attack, defense, ability1, ability2) {
     this.name = name
     this.hp = hp
     this.attack = attack
     this.defense = defense
-    this.abilities = abilities
+    this.ability1 = ability1
+    this.ability2 = ability2
   }
 }
 
-// selecting ul from the html side
-let ul = document.querySelector("ul")
-let pokemon = new Pokemon()
-
+//let crobat;
 // retrieve data from hosted person-1 json file
-axios.get("http://pokeapi.co/api/v2/pokemon/169/")
-.then((response) => {
-  // creates new li element
-  let li = document.createElement("li")
-  let data = response.data
+axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.json")
+  .then((response) => {
+    // creates new li element
+    let data = response.data
+    let section = document.querySelector("#crobat")
+    let name = document.querySelector(".name1")
+    let hp = document.querySelector(".hp1")
+    let attack = document.querySelector(".attack1")
+    let defense = document.querySelector(".defense1")
+    let ability1 = document.querySelector(".abi1-1")
+    let ability2 = document.querySelector(".abi1-2")
 
-  // creates new Person object
-  // based on JSON payload
-  let crobat = new Pokemon(
-    data.name,
-    data.stats[5].base_stat,
-    data.stats[4].base_stat,
-    data.stats[3].base_stat,
-    data.abilities[0].ability.name,
-    data.abilities[1].ability.name,
-)
 
-  console.log(crobat)
-  // adds new Person object
-  pokemon.add(crobat)
+    crobat = new Pokemon(
+      data.name,
+      data.stats[5].base_stat,
+      data.stats[4].base_stat,
+      data.stats[3].base_stat,
+      data.abilities[0].ability.name,
+      data.abilities[1].ability.name,
+    )
 
-  // changes inner html of li to first name
-  li.innerHTML = crobat
-    
-  // appends created li to the html ul
-  ul.appendChild(li)
-}).catch((error) => {
-  console.log(error)
-})
+    pokeAll.push(crobat);
+
+    name.innerHTML = data.name;
+    hp.innerHTML = "HP: " + data.stats[5].base_stat;
+    attack.innerHTML = "ATTK:" + " " + data.stats[4].base_stat
+    defense.innerHTML = "DFNS: " + data.stats[3].base_stat
+    ability1.innerHTML = data.abilities[0].ability.name
+    ability2.innerHTML = data.abilities[1].ability.name
+
+    //counter to keep track of pokemon in array, instead of random
+    let index = pokeAll.length - 1;
+
+    // add event listener and pointer class inside of axios
+    crobatTarget.addEventListener("click", function () {
+      changePokemon(index)
+    });
+    crobatTarget.classList.add("pointer");
+  
+  }).catch((error) => {
+    console.log(error)
+  })
+
+
+// })
+// creates new Person object
 
 // retrieve data from hosted person-2 json file
-axios.get("https://github.com/tonymacas/pokedex/blob/master/croconaw.json")
-.then((response) => {
-  // creates new li element
-  let li = document.createElement("li")
-  let data = response.data
+axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/croconaw.json")
+  .then((response) => {
+    // creates new li element
 
-  // creates new Person object
-  // based on JSON payload
-  let croconaw = new Pokemon(
-    data.name,
-    data.stats[5].base_stat,
-    data.stats[4].base_stat,
-    data.stats[3].base_stat,
-    data.abilities[0].ability.name,
-    data.abilities[1].ability.name,
-)
 
-  // adds new Person object
-  pokemon.add(croconaw)
+    let data = response.data
+    let section = document.querySelector("#croconaw")
+    let name = document.querySelector(".name2")
+    let hp = document.querySelector(".hp2")
+    let attack = document.querySelector(".attack2")
+    let defense = document.querySelector(".defense2")
+    let ability1 = document.querySelector(".abi2-1")
+    let ability2 = document.querySelector(".abi2-2")
 
-  // changes inner html of li to first name
-  li.innerHTML = croconaw
-    
-  // appends created li to the html ul
-  ul.appendChild(li)
-}).catch((error) => {
-  console.log(error)
-})
+
+
+    // creates new Person object
+    // based on JSON payload
+    let croconaw = new Pokemon(
+      data.name,
+      data.stats[5].base_stat,
+      data.stats[4].base_stat,
+      data.stats[3].base_stat,
+      data.abilities[0].ability.name,
+      data.abilities[1].ability.name,
+    )
+
+    pokeAll.push(croconaw);
+
+    name.innerHTML = data.name;
+    hp.innerHTML = "HP: " + data.stats[5].base_stat;
+    attack.innerHTML = "ATTK:" + " " + data.stats[4].base_stat
+    defense.innerHTML = "DFNS: " + data.stats[3].base_stat
+    ability1.innerHTML = data.abilities[0].ability.name
+    ability2.innerHTML = data.abilities[1].ability.name
+  
+
+    //counter to keep track of pokemon in array, instead of random
+    let index = pokeAll.length - 1;
+    // add event listener and pointer class inside of axios
+
+    croconawTarget.addEventListener("click", function () {
+      changePokemon(index)
+    });
+    croconawTarget.classList.add("pointer");
+
+  }).catch((error) => {
+    console.log(error)
+  })
 
 // retrieve data from hosted person-3 json file
-axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.json#")
-.then((response) => {
-  // creates new li element
-  let li = document.createElement("li")
-  let data = response.data
+axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.json")
+  .then((response) => {
+    // creates new li element
+    let data = response.data
+    let section = document.querySelector("#crustle")
+    let name = document.querySelector(".name3")
+    let hp = document.querySelector(".hp3")
+    let attack = document.querySelector(".attack3")
+    let defense = document.querySelector(".defense3")
+    let ability1 = document.querySelector(".abi3-1")
+    let ability2 = document.querySelector(".abi3-2")
+    let ability3 = document.querySelector(".abi3-3")
 
-  // creates new Person object
-  // based on JSON payload
-  let crustle = new Pokemon(
-    data.name,
-    data.stats[5].base_stat,
-    data.stats[4].base_stat,
-    data.stats[3].base_stat,
-    data.abilities[0].ability.name,
-    data.abilities[1].ability.name,
-    data.abilities[2].ability.name
-)
+    // creates new Pokemon object
+    // based on JSON payload
+    let crustle = new Pokemon(
+      data.name,
+      data.stats[5].base_stat,
+      data.stats[4].base_stat,
+      data.stats[3].base_stat,
+      data.abilities[0].ability.name,
+      data.abilities[1].ability.name,
+      data.abilities[2].ability.name,
+    )
 
-  // adds new Person object
-  pokemon.add(crustle)
+    pokeAll.push(crustle);
 
-  // changes inner html of li to first name
-  li.innerHTML = crustle
-    
-  // appends created li to the html ul
-  ul.appendChild(li)
-}).catch((error) => {
-  console.log(error)
-})
+    name.innerHTML = data.name;
+    hp.innerHTML = "HP: " + data.stats[5].base_stat;
+    attack.innerHTML = "Attk:" + " " + data.stats[4].base_stat
+    defense.innerHTML = "Def: " + data.stats[3].base_stat
+    ability1.innerHTML = data.abilities[0].ability.name
+    ability2.innerHTML = data.abilities[1].ability.name
+    ability3.innerHTML = data.abilities[2].ability.name
+
+    //counter to keep track of pokemon in array, instead of random
+    let index = pokeAll.length - 1;
+    // add event listener and pointer class inside of axios
+    crustleTarget.addEventListener("click", function () {
+      changePokemon(index)
+    });
+    crustleTarget.classList.add("pointer");
+    console.log(index);
+  }).catch((error) => {
+    console.log(error)
+  })
+
+///one function that changes pokemon depending on number passed through
+function changePokemon(number) {
+  displayedPokemon.src = `./pokemon/${pokeAll[number].name}.gif`
+  pokeName.innerHTML = pokeAll[number].name;
+  pokeHp1.innerHTML = pokeAll[number].hp;
+  pokeAttack1.innerHTML = pokeAll[number].attack;
+  pokeDefense1.innerHTML = pokeAll[number].defense;
+  pokeAbi1.innerHTML = pokeAll[number].ability1;
+  pokeAbi2.innerHTML = pokeAll[number].ability2;
+}
+
+
+
+
+
+// function changePoke2() {
+//   let pokeImage = document.querySelector(".poke-image")
+//   if (pokeAll[0].name === "croconaw") {
+//   pokeImage.src = "pokemon/croconaw.gif";
+//   } else if (pokeAll[0].name === "crobat") {
+//     pokeImage.src = "pokemon/crobat.gif";
+//   } else {
+//     pokeImage.src = "pokemon/crustle.gif";
+//   }
+//   let pokeName = document.querySelector(".name1");
+//   pokeName.innerHTML = pokeAll[0].name;
+//   let pokeHp1 = document.querySelector(".hp1");
+//   pokeHp1.innerHTML = pokeAll[0].hp;
+//   let pokeAttack1 = document.querySelector(".attack1")
+//   pokeAttack1.innerHTML = pokeAll[0].attack;
+//   let pokeDefense1 = document.querySelector(".defense1");
+//   pokeDefense1.innerHTML = pokeAll[0].defense;
+//   let pokeAbi1 = document.querySelector(".abi1-1");
+//   pokeAbi1.innerHTML =pokeAll[0].ability;
+//   let pokeAbi2 = document.querySelector(".abi1-2");
+//   pokeAbi2.innerHTML = pokeAll[0].ability;
+// }
+
+
+// function changePoke1() {
+//   let pokeImage = document.querySelector(".poke-image")
+//   if (pokeAll[0].name === "croconaw") {
+//   pokeImage.src = "pokemon/croconaw.gif";
+//   } else if (pokeAll[0].name === "crobat") {
+//     pokeImage.src = "pokemon/crobat.gif";
+//   } else {
+//     pokeImage.src = "pokemon/crustle.gif";
+//   }
+//   let pokeName = document.querySelector(".name1");
+//   pokeName.innerHTML = pokeAll[0].name;
+//   let hp = document.querySelector(".hp1");
+//   pokeHp1.innerHTML = pokeAll[0].hp;
+//   let pokeAttack1 = document.querySelector(".attack1")
+//   pokeAttack1.innerHTML = pokeAll[0].attack;
+//   let pokeDefense1 = document.querySelector(".defense1");
+//   pokeDefense1.innerHTML = pokeAll[0].defense;
+//   let pokeAbi1 = document.querySelector(".abi1-1");
+//   pokeAbi1.innerHTML =pokeAll[0].ability;
+//   let pokeAbi2 = document.querySelector(".abi1-2");
+//   pokeAbi2.innerHTML = pokeAll[1].ability;
+//   let pokeAbi3
+// }
