147d9310dffb35bce4cdd99338cea38d2169a1fc
Anthony Macas <36384104+tonymacas@users.noreply.github.com>
2018-04-26 00:19:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-// global variable for pokemon
 let pokeAll = [];
 
-
-//making variables to capture and fill info im getting
 let displayedPokemon = document.querySelector(".poke-image");
 let pokeName = document.querySelector(".name1");
 let pokeHp1 = document.querySelector(".hp1");
@@ -16,16 +13,11 @@ let croconawTarget = document.querySelector("#secondball");
 let crustleTarget = document.querySelector('#thirdball');
 
 
-
-// container class
 class Tony {
   constructor() {
     this.all = []
   }
 
-  // defines method add to People class
-  // it accepts a Person object and adds it to the end
-  // of the "all" array
   add(pokeData) {
     this.all.push(pokeData)
   }
@@ -40,7 +32,6 @@ get(pokemonName) {
   }
 }
 
-// individual class
 class Pokemon {
   constructor(name, hp, attack, defense, ability1, ability2) {
     this.name = name
@@ -52,11 +43,9 @@ class Pokemon {
   }
 }
 
-//let crobat;
-// retrieve data from hosted pokemon-1 api
 axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.json")
   .then((response) => {
-    // creates new li element(this is where i will fill my info from the API)
+   
     let data = response.data
     
     let section = document.querySelector("#crobat")
@@ -67,8 +56,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.jso
     let ability1 = document.querySelector(".abi1-1")
     let ability2 = document.querySelector(".abi1-2")
 
-
-    //making the new pokemon and position the info I took from Pokemon API
     crobat = new Pokemon(
       data.name,
       data.stats[5].base_stat,
@@ -80,8 +67,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.jso
 
     pokeAll.push(crobat);
 
-
-    //using the info and displaying it in the HTML file 
     name.innerHTML = data.name;
     hp.innerHTML = "HP: " + data.stats[5].base_stat;
     attack.innerHTML = "Attk:" + " " + data.stats[4].base_stat
@@ -89,10 +74,8 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.jso
     ability1.innerHTML = data.abilities[0].ability.name
     ability2.innerHTML = data.abilities[1].ability.name
 
-    //counter to keep track of pokemon in array, instead of random
     let index = pokeAll.length - 1;
 
-    // add event listener and pointer class inside of axios
     crobatTarget.addEventListener("click", function () {
       changePokemon(index)
     });
@@ -102,16 +85,9 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crobat.jso
     console.log(error)
   })
 
-
-// })
-// creates new Person object
-
-// retrieve data from hosted person-2 json file
 axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/croconaw.json")
   .then((response) => {
-    // creates new li element
-
-
+  
     let data = response.data
     let section = document.querySelector("#croconaw")
     let name = document.querySelector(".name2")
@@ -121,10 +97,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/croconaw.j
     let ability1 = document.querySelector(".abi2-1")
     let ability2 = document.querySelector(".abi2-2")
 
-
-
-    // creates new Person object
-    // based on JSON payload
     let croconaw = new Pokemon(
       data.name,
       data.stats[5].base_stat,
@@ -143,10 +115,7 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/croconaw.j
     ability1.innerHTML = data.abilities[0].ability.name
     ability2.innerHTML = data.abilities[1].ability.name
 
-
-    //counter to keep track of pokemon in array, instead of random
     let index = pokeAll.length - 1;
-    // add event listener and pointer class inside of axios
 
     croconawTarget.addEventListener("click", function () {
       changePokemon(index)
@@ -157,7 +126,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/croconaw.j
     console.log(error)
   })
 
-// retrieve data from hosted person-3 json file
 axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.json")
   .then((response) => {
     // creates new li element
@@ -171,8 +139,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.js
     let ability2 = document.querySelector(".abi3-2")
     let ability3 = document.querySelector(".abi3-3")
 
-    // creates new Pokemon object
-    // based on JSON payload
     let crustle = new Pokemon(
       data.name,
       data.stats[5].base_stat,
@@ -193,7 +159,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.js
     ability2.innerHTML = data.abilities[1].ability.name
     ability3.innerHTML = data.abilities[2].ability.name
 
-    //counter to keep track of pokemon in array, instead of random
     let index = pokeAll.length - 1;
     // add event listener and pointer class inside of axios
     crustleTarget.addEventListener("click", function () {
@@ -205,7 +170,6 @@ axios.get("https://raw.githubusercontent.com/tonymacas/pokedex/master/crustle.js
     console.log(error)
   })
 
-///one function that changes pokemon depending on number passed through
 function changePokemon(number) {
   displayedPokemon.src = `./pokemon/${pokeAll[number].name}.gif`
   pokeName.innerHTML = pokeAll[number].name;
